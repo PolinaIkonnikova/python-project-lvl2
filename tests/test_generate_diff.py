4880b604698bfe98bb157fa4616e32191dd446da
Polina Ikonnikova <spollyv@gmail.com>
2022-03-16 22:16:25 +0500

    fixed tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,59 +2,55 @@ import json
 from gendiff import generate_diff
 
 
-result1 = open('tests/fixtures/result1_nothing_to_change.txt', 'r').read()
-result2 = open('tests/fixtures/result2_flatfile.txt', 'r').read()
-result3 = open('tests/fixtures/result3_stylish_treefile.txt', 'r').read()
-result4 = open('tests/fixtures/result4_plain_treefile.txt', 'r').read()
-result5 = open('tests/fixtures/result5_json_treefile.txt', 'r').read()
-
-
 def test_flat_file():
-    result1 = open('tests/fixtures/result1_nothing_to_change.txt', 'r').read()
-    result2 = open('tests/fixtures/result2_flatfile.txt', 'r').read()
-    assert generate_diff('tests/fixtures/file1.json',
-                         'tests/fixtures/file1.json') == result1
-    assert generate_diff('tests/fixtures/file1.json',
-                         'tests/fixtures/file2.json') == result2
-    assert generate_diff('tests/fixtures/file1.yaml',
-                         'tests/fixtures/file2.yaml') == result2
+    result1 = open('tests/fixtures/nothing_to_change.txt', 'r').read()
+    result2 = open('tests/fixtures/flatfile_stylish.txt', 'r').read()
+    assert generate_diff('tests/fixtures/flat1.json',
+                         'tests/fixtures/flat1.json') == result1
+    assert generate_diff('tests/fixtures/flat1.json',
+                         'tests/fixtures/flat2.json') == result2
+    assert generate_diff('tests/fixtures/flat1.yaml',
+                         'tests/fixtures/flat2.yaml') == result2
 
 
 def test_gendiff_stylish():
-    assert generate_diff('tests/fixtures/dict1.json',
-                         'tests/fixtures/dict2.json') == result3
-    assert generate_diff('tests/fixtures/dict1.yaml',
-                         'tests/fixtures/dict2.yaml') == result3
+    result3 = open('tests/fixtures/stylish_treefile.txt', 'r').read()
+    assert generate_diff('tests/fixtures/tree1.json',
+                         'tests/fixtures/tree2.json') == result3
+    assert generate_diff('tests/fixtures/tree1.yaml',
+                         'tests/fixtures/tree2.yaml') == result3
 
 
 def test_gendiff_plain():
-    assert generate_diff('tests/fixtures/dict1.json',
-                         'tests/fixtures/dict2.json',
+    result4 = open('tests/fixtures/plain_treefile.txt', 'r').read()
+    assert generate_diff('tests/fixtures/tree1.json',
+                         'tests/fixtures/tree2.json',
                          'plain') == result4
-    assert generate_diff('tests/fixtures/dict1.yaml',
-                         'tests/fixtures/dict2.yaml',
+    assert generate_diff('tests/fixtures/tree1.yaml',
+                         'tests/fixtures/tree2.yaml',
                          'plain') == result4
-    assert not generate_diff('tests/fixtures/dict1.yaml',
-                             'tests/fixtures/dict2.yaml') == result4
+    assert not generate_diff('tests/fixtures/tree1.yaml',
+                             'tests/fixtures/tree2.yaml') == result4
 
 
 def is_json(json_data):
     try:
-        json_object = json.loads(json_data)
+        json.loads(json_data)
     except ValueError:
         return False
     return True
 
 
 def test_gendiff_json():
-    json_data_1 = generate_diff('tests/fixtures/dict1.json',
-                                'tests/fixtures/dict2.json',
+    result1 = generate_diff('tests/fixtures/tree1.json',
+                                'tests/fixtures/tree2.json',
                                 'json')
-    json_data_2 = generate_diff('tests/fixtures/file1.yaml',
-                                'tests/fixtures/file2.yaml',
+    result2 = generate_diff('tests/fixtures/flat1.yaml',
+                                'tests/fixtures/flat2.yaml',
                                 'json')
-    json_data_3 = generate_diff('tests/fixtures/dict1.yaml',
-                                'tests/fixtures/dict2.yaml')
-    assert is_json(json_data_1) is True
-    assert is_json(json_data_2) is True
-    assert is_json(json_data_3) is False
+    result3 = generate_diff('tests/fixtures/tree1.yaml',
+                                'tests/fixtures/tree2.yaml')
+    assert is_json(result1) == True
+    assert is_json(result2) == True
+    assert is_json(result3) == False
+
