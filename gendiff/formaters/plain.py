095e39b08f164b6a6280e6efb11ec331cbdcf21a
Polina Ikonnikova <spollyv@gmail.com>
2022-04-09 23:23:17 +0500

    for cclimate

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -10,9 +10,6 @@ def get_value(val):
     if isinstance(val, bool) or val is None:
         return json.dumps(val)
 
-    if isinstance(val, int) or isinstance(val, float):
-        return str(val)
-
     return "'{}'".format(str(val))
 
 
@@ -31,23 +28,24 @@ def make_format(diff_dict):
 
         if status == 'internal_change':
             children = attributes['value']
-            return '\n'.join(list(map(
-                             lambda item: make_lines(item, path_record),
-                             get_dict(children).items())))
+            new_line = '\n'.join(list(map(
+                                 lambda item: make_lines(item, path_record),
+                                 get_dict(children).items())))
 
         elif status == 'added':
             val = attributes['value']
-            return "Property '{}' was added with value: {}".format(
+            new_line = "Property '{}' was added with value: {}".format(
                 '.'.join(path_record), get_value(val))
 
         elif status == 'deleted':
-            return "Property '{}' was removed".format('.'.join(path_record))
+            new_line = "Property '{}' was removed".format('.'.join(path_record))
 
         elif status == 'changed_value':
             del_val, add_val = attributes['value']
-            return "Property '{}' was updated. From {} to {}".format(
-                '.'.join(path_record), get_value(del_val),
-                get_value(add_val))
+            new_line = "Property '{}' was updated. From {} to {}".format(
+                '.'.join(path_record), get_value(del_val), get_value(add_val))
+
+        return new_line
 
     return '\n'.join(list(map(lambda item: make_lines(item, path),
                               get_dict(diff_dict).items())))
